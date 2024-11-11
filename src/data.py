350bba59ff01b6aa368e17b360a1e02d520c3862
StefaniaLavarda <stefanialavarda9@gmail.com>
2024-11-11 19:06:01 +0100

    Add class MoviesData to read CSV file in data.py

---
+++ src/data.py
@@ -0,0 +1,22 @@
+""" src/data.py """
+
+import os
+import pandas as pd
+
+class MoviesData:
+    """" A class for managing and importing movie data from a CSV file.
+
+    Parameters:
+    ----------
+    path (str): 
+        A path pointing to the folder in which the movie data file is stored .
+    """
+    # initializes the MoviesData instance with the given path.
+    def __init__(self, path):
+        self.path = os.path.join(path, "")
+
+    # read a CSV file using pandas and returns it as a DataFrame.
+    def import_data(self, file_path):
+        df = pd.read_csv(file_path, sep=",")
+        return df
+    
