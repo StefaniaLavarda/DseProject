83e8cd488cd0b94881fe01d3e894ffaf08ef5370
StefaniaLavarda <stefanialavarda9@gmail.com>
2024-11-12 10:03:56 +0100

    create the method question_year

--- src/quiz.py
+++ src/quiz.py
@@ -1,5 +1,7 @@
 # src/quiz.py
 
+import numpy as np
+
 class Quiz:
     """
        A class for generating random questions, with four options, one of which 
@@ -7,3 +9,25 @@ class Quiz:
     """
     def __init__(self, data):
         self.data = data
+  
+    def question_year(self,data):
+        """
+            Generates a question asking for the release year of a randomly chosen movie.
+        """
+        # choose a random movie and select, from the df, the title and year of the movie
+        question_row = data.sample(1)
+        primary_title = question_row['primaryTitle'].value[0]
+        start_year = question_row['startYear'].value[0]
+
+        # create the question string using the selected movie's title
+        question = f"What year was the movie {primary_title} released?"
+        correct_answer = start_year
+       
+        # generate multiple choices options
+        options = set()
+        options.add(correct_answer)
+        while len(options) < 4:
+            random_year = np.random.choice(range(1894, 2024))
+            options.add(random_year)
+        
+        return question, list(options), correct_answer
