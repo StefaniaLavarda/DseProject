017581b7b56f7739ef1e3d3226a58550ffacf9c4
StefaniaLavarda <stefanialavarda9@gmail.com>
2024-11-16 11:49:01 +0100

    Add a list to collect user answers.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ quiz = Quiz(movies_df)
 score_by_difficulty = {'easy': 1, 'medium': 2, 'hard': 3}
 
 # Title
-st.set_page_config(page_title="Movie Quiz Game", page_icon="🎞️", layout="centered")
+st.title("Movie Quiz Game 🎞️")
 
 # Instructions
 st.write("Answer 10 multiple-choice questions about movies. Choose the correct option!")
@@ -40,16 +40,22 @@ for i in range(10):
     correct_answers.append(correct_answer)
     difficulties.append(level_of_difficulty)
 
+# List to collect user answers
+user_answers = []
+
 # Initialize a counter for the question number
 question_number = 1
 
 # Display each question with options
 for question, options in questions:
     st.subheader(f"Question {question_number}: {question}")
-    user_answer = st.radio(f"Select your answer for Question {question_number}:", options,)
+    user_answer = st.radio(
+        f"Select your answer for Question {question_number}:", 
+        options,
+    )
+    user_answers.append(user_answer)
     
     # Increment the counter for the next question
     question_number += 1 
 
- 
 
